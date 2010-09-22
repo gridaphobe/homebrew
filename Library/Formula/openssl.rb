3bb85824dedfda15574d2fa59544bd3c4b794025
Eric Seidel <gridaphobe@gmail.com>
2010-09-21 22:04:24 -0400

    update to version 1.0.0a

--- Library/Formula/openssl.rb
+++ Library/Formula/openssl.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class Openssl <Formula
-  url 'http://www.openssl.org/source/openssl-0.9.8o.tar.gz'
-  version '0.9.8o'
+  url 'http://www.openssl.org/source/openssl-1.0.0a.tar.gz'
+  version '1.0.0a'
   homepage 'http://www.openssl.org'
-  md5 '63ddc5116488985e820075e65fbe6aa4'
+  md5 'e3873edfffc783624cfbdb65e2249cbd'
 
   keg_only :provided_by_osx
 
   def install
     ENV.j1 # Breaks on Mac Pro
-    system "./config", "--prefix=#{prefix}",
+    system "./Configure", "darwin64-x86_64-cc", "--prefix=#{prefix}",
                        "--openssldir=#{etc}",
                        "zlib-dynamic", "shared"
     system "make"

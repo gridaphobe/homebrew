7c2e669a60edf93fbb92fcbd09e9bc26a9b68141
Eric Seidel <gridaphobe@gmail.com>
2010-09-21 22:03:07 -0400

    update to new patch also compile for c++ and fortran

--- Library/Formula/hdf5.rb
+++ Library/Formula/hdf5.rb
@@ -1,14 +1,15 @@
 require 'formula'
 
 class Hdf5 <Formula
-  url 'http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.5.tar.bz2'
+  url 'http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.5-patch1.tar.gz'
   homepage 'http://www.hdfgroup.org/HDF5/'
-  md5 'a400fe6c1c5964a7224dc684225d415c'
+  md5 'd22a35a17877e369055ed50da5ba91fa'
+  version '1.8.5-patch1'
 
   depends_on 'szip'
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--enable-cxx", "--enable-fortran", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
 end
